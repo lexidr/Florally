16d5d712e299e04ce31320d2a9149e2fa3a2b525
george <georgiibezk@gmail.com>
2025-11-24 18:07:49 +0300

    Add files via upload

---
+++ SectionRegistrationForm.jsx
@@ -0,0 +1,117 @@
+
+//Мы старались 2 дня, надеемся, что вам понравится, если что-то не так, то сразу напишите (￣o￣) . z Z
+
+import './SectionRegistrationForm.css';
+
+const RegistrationForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Форма отправлена');
+    
+    if (e.target.checkValidity()) {
+      window.location.href = 'ССЫЛКА НА САЙТ';// o(*￣︶￣*)o
+    }
+  };
+
+  const handleLoginClick = (e) => {
+    e.preventDefault();
+    window.location.href = '/login';//d=====(￣▽￣*)b
+  };
+
+  const buttonContainerStyle = {
+    margin: '1.2vh'
+  };
+
+  const linkContainerStyle = {
+    margin: '1vh'
+  };
+
+  const linkTextStyle = {
+    fontSize: '1.7vh'
+  };
+
+  const logoStyle = {
+    position: 'absolute',
+    bottom: '20px',
+    left: '50%',
+    transform: 'translateX(-50%)'
+  };
+
+  return (
+    <section className="registration-container">
+      <div className="form-section">
+        <div className="registration-card">
+          <h2>Регистрация</h2>
+          <form 
+            className="registration-form" 
+            onSubmit={handleSubmit}
+          >
+            <div>
+              <label htmlFor="name" className="visually-hidden">Полное имя</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Имя"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="mail" className="visually-hidden">Email</label>
+              <input
+                type="email"
+                id="mail"
+                placeholder="Почта"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="visually-hidden">Пароль</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Пароль"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="confirmPassword" className="visually-hidden">Повторите пароль</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="Повторите пароль"
+                required
+              />
+            </div>
+            <div style={buttonContainerStyle}>
+              <button 
+                type="submit" 
+                className="registration-buttom" 
+                style={{width: '76%', margin: '0 auto'}}
+              >
+                Зарегистрироваться
+              </button>
+            </div>
+            <div style={linkContainerStyle}>
+              <span style={linkTextStyle} className="login-link">
+                Есть аккаунт? {' '}
+                <a href="/login" onClick={handleLoginClick}>
+                  Войти
+                </a>
+              </span>
+            </div>
+          </form>
+          <img 
+            style={logoStyle} 
+            src="logo.svg" 
+            alt="logo" 
+          />
+        </div>
+      </div>
+      <div className="image-section">
+        <img src="OBJECTS.svg" alt="Background" className="background-image" />
+      </div>
+    </section>
+  );
+};
+
+export default RegistrationForm;
