85e95211abddf0bdae256f5f08f27fa50502afa3
georgebez <georgiibezk@gmail.com>
2025-12-01 16:53:31 +0300

    Добавлена мобильная верстка

--- vite-project/src/components/SectionRegistrationForm/SectionRegistrationForm.jsx
+++ vite-project/src/components/SectionRegistrationForm/SectionRegistrationForm.jsx
@@ -4,26 +4,24 @@ import { Link } from 'react-router-dom';
 const RegistrationForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Форма отправлена');
+    console.log("Форма отправлена");
+
+    if (e.target.checkValidity()) {
+      window.location.href = "ССЫЛКА НА САЙТ";
+    }
+  };
+
+  const handleLoginClick = (e) => {
+    e.preventDefault();
+    window.location.href = "/login";
   };
 
   const buttonContainerStyle = {
-    margin: '1.2vh'
+    margin: "1.2vh",
   };
 
   const linkContainerStyle = {
-    margin: '1vh'
-  };
-
-  const linkTextStyle = {
-    fontSize: '1.7vh'
-  };
-
-  const logoStyle = {
-    position: 'absolute',
-    bottom: '20px',
-    left: '50%',
-    transform: 'translateX(-50%)'
+    margin: "1vh",
   };
 
   return (
@@ -33,25 +31,21 @@ const RegistrationForm = () => {
           <h2>Регистрация</h2>
           <form className="registration-form" onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="name" className="visually-hidden">Полное имя</label>
-              <input
-                type="text"
-                id="name"
-                placeholder="Имя"
-                required
-              />
+              <label htmlFor="name" className="visually-hidden">
+                Полное имя
+              </label>
+              <input type="text" id="name" placeholder="Имя" required />
             </div>
             <div>
-              <label htmlFor="mail" className="visually-hidden">Email</label>
-              <input
-                type="email"
-                id="mail"
-                placeholder="Почта"
-                required
-              />
+              <label htmlFor="mail" className="visually-hidden">
+                Email
+              </label>
+              <input type="email" id="mail" placeholder="Почта" required />
             </div>
             <div>
-              <label htmlFor="password" className="visually-hidden">Пароль</label>
+              <label htmlFor="password" className="visually-hidden">
+                Пароль
+              </label>
               <input
                 type="password"
                 id="password"
@@ -60,7 +54,9 @@ const RegistrationForm = () => {
               />
             </div>
             <div>
-              <label htmlFor="confirmPassword" className="visually-hidden">Повторите пароль</label>
+              <label htmlFor="confirmPassword" className="visually-hidden">
+                Повторите пароль
+              </label>
               <input
                 type="password"
                 id="confirmPassword"
@@ -69,30 +65,34 @@ const RegistrationForm = () => {
               />
             </div>
             <div style={buttonContainerStyle}>
-              <button 
-                type="submit" 
-                className="registration-buttom" 
-                style={{width: '76%', margin: '0 auto'}}
+              <button
+                type="submit"
+                className="registration-buttom"
+                style={{ width: "76%", margin: "0 auto" }}
               >
                 Зарегистрироваться
               </button>
             </div>
-            <div style={linkContainerStyle}>
-              <span style={linkTextStyle} className="login-link">
-                Есть аккаунт? {' '}
-                <Link to="/auth/signin">Войти</Link>
+            <div className="login-link">
+              <span>
+                Есть аккаунт?{" "}
+                <a href="/login" onClick={handleLoginClick}>
+                  Войти
+                </a>
               </span>
             </div>
+            <div className="logoStyle">
+              <img src="logo.svg" alt="logo" />
+            </div>
           </form>
-          <img 
-            style={logoStyle} 
-            src="/logo.svg" 
-            alt="logo" 
-          />
         </div>
       </div>
       <div className="image-section">
-        <img src="/back-img.svg" alt="Девушка поливает цветок в горшке" className="background-image" />
+        <img
+          src="OBJECTS.svg"
+          alt="Девушка поливает цветок в горшке"
+          className="background-image"
+        />
       </div>
     </section>
   );
